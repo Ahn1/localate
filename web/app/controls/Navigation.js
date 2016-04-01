a77e94bbb2f171657b93244d527a400b4dc4c62e
Alexander Bogoslawski <alexander.bogoslawski@web.de>
2016-04-01 14:59:26 +0200

    add sample nav control

---
+++ web/app/controls/Navigation.js
@@ -0,0 +1,23 @@
+import {Component, PropTypes} from 'react';
+
+export default class Navigation extends Component {
+
+  render() {
+    return (
+      <ul class="navigation">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    );
+  }
+}
