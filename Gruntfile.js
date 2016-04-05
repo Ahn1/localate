27bbd4aa8940af2d89182ec8eb0fd5d32f7d4396
Alexander Bogoslawski <bogoslawski@landaumedia.de>
2016-04-05 08:47:09 +0200

    Add watch for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,79 @@
 module.exports = function(grunt) {
 
-  require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-    babel: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'server/',
-          src: ['**/*.js'],
-          dest: 'dist/server/'
-        }]
-      }
-    },
-    webpack: {
-      dist: {
-        entry: "./web/app/entry.js",
-        output: {
-          path: "dist/web/",
-          filename: "app.js",
+    grunt.initConfig({
+        babel: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'server/',
+                    src: ['**/*.js'],
+                    dest: 'dist/server/'
+                }]
+            }
         },
-        stats: {
-          // Configure the console output
-          colors: true,
-          modules: true,
-          reasons: true
+        webpack: {
+            dist: {
+                entry: "./web/app/entry.js",
+                output: {
+                    path: "dist/web/",
+                    filename: "app.js",
+                },
+                stats: {
+                    // Configure the console output
+                    colors: true,
+                    modules: true,
+                    reasons: true
+                },
+                module: {
+                    loaders: [{
+                        test: /\.jsx?$/,
+                        exclude: /(node_modules|bower_components)/,
+                        loader: 'babel'
+                    }]
+                },
+                resolve: {
+                    extensions: ['', '.js']
+                }
+            }
         },
-        module: {
-          loaders: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel'
-          }]
+
+        postcss: {
+            options: {
+                map: true,
+
+                processors: [
+                    require("postcss-import")(),
+                    require('postcss-center'),
+                    require("postcss-cssnext")(),
+                    //require('cssnano')() // minify the result
+                ]
+            },
+            dist: {
+                src: 'web/style/index.css',
+                dest: 'dist/style/main.css'
+            }
         },
-        resolve: {
-          extensions: ['', '.js']
-        }
-      }
-    },
 
-    postcss: {
-      options: {
-        map: true,
+        watch: {
+            options: {
+                spawn: true,
+            },
+            css: {
+                files: ['web/style/**/*.css'],
+                tasks: ['postcss']
+            },
+            js: {
+                files: ['web/app/**/*.js'],
+                tasks: ['webpack']
+            }
+        },
 
-        processors: [
-          require("postcss-import")(),
-          require('postcss-center'),
-          require("postcss-cssnext")(),
-          require('cssnano')() // minify the result
-        ]
-      },
-      dist: {
-        src: 'web/style/index.css',
-        dest: 'dist/style/main.css'
-      }
-    }
+    });
 
-  });
-
-  grunt.registerTask('server', ['babel']);
-  grunt.registerTask('default', ['babel', 'webpack', 'postcss']);
-  grunt.registerTask('web', ['webpack', 'postcss']);
+    grunt.registerTask('server', ['babel']);
+    grunt.registerTask('default', ['babel', 'webpack', 'postcss']);
+    grunt.registerTask('web', ['webpack', 'postcss']);
 
 };
