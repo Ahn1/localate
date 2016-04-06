6e063946f093e2d0766d9aea3073138c944b3cf5
Alexander Bogoslawski <bogoslawski@landaumedia.de>
2016-04-06 11:24:30 +0200

    Add Wait for event methods

--- web/app/stores/Dispatcher.js
+++ web/app/stores/Dispatcher.js
@@ -1,5 +1,26 @@
 import {Dispatcher} from 'flux';
 
-module.exports = new Dispatcher({
-  logLevel: 'ALL'
-});
+export default new class MyDispatcher extends Dispatcher{
+  constructor(){
+    super();
+
+    this.register((e) => this.HandleInternal(e))
+    console.log(this);
+  }
+
+  HandleInternal(e){
+
+  }
+
+  async WaitForEvent(eventType){
+    return new Promise((res,rej) => {
+      let regId = this.register((e) => {
+        if(eventType == e.type)
+        {
+          this.unregister(regId);
+          res(e);
+        }
+      });
+    });
+  }
+}();
--- web/app/stores/StoreBase.js
+++ web/app/stores/StoreBase.js
@@ -1,5 +1,5 @@
 import { EventEmitter } from 'events';
-var AppDispatcher = require('./Dispatcher.js');
+import AppDispatcher from './Dispatcher.js';
 
 export default class StoreBase extends EventEmitter {
 
@@ -11,6 +11,17 @@ export default class StoreBase extends EventEmitter {
     AppDispatcher.register((payload) => {
       if(payload.type == method)
         action();
+    });
+  }
+
+  async WaitForEvent(eventType){
+    return new Promise((res,rej) => {
+      console.log("CREATE PROMISE")
+      let callback = (e) => {
+        this.removeListener(eventType, callback);
+        res(e);
+      }
+      this.on(eventType,callback);
     });
   }
 
