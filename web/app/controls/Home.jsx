966586b632ec4b3f1a8bf33eaf09181bf25d3774
Alexander Bogoslawski <Alexander.bogoslawski@web.de>
2016-04-14 19:14:27 +0200

    Dino hinzufügen

--- web/app/controls/Home.jsx
+++ web/app/controls/Home.jsx
@@ -6,7 +6,10 @@ export default class Home extends Component {
     render() {
         return (
             <DocumentTitle title='Home'>
-                <div>Hallo</div>
+                <div>
+                    <h1>Roaawrrr!!!</h1>
+                    <img className="dino" src="http://thumbs.dreamstime.com/z/funny-dinosaur-coloring-pages-high-quality-hand-drawing-illustration-picture-you-can-see-dino-great-picture-all-your-67659150.jpg"/>
+                </div>
             </DocumentTitle>
         );
     }
