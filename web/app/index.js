c4314a0f38c16505aa6a95f2a7a1e26a24d39481
Alexander Bogoslawski <bogoslawski@landaumedia.de>
2016-07-05 12:20:06 +0200

    Use redux-thunk

--- web/app/index.js
+++ web/app/index.js
@@ -1,12 +1,14 @@
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore,applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
 
 import appReducers from './reducers'
-
 import App from './Views/App'
 
-let store = createStore(appReducers)
+
+let store = createStore(appReducers,applyMiddleware(thunk))
+
 
 render(
   <Provider store={store}>
